a20d3df8e2b8570a37f6092510a487e5a4ad452e
Rodrigo Werlich <rodrigowerlich@clincard.com.br>
2019-11-04 16:38:53 -0200

    testes

---
+++ src/Carrinho.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+
+class Carrinho
+{
+
+    private $produtos = [];
+    public function addProduto($produto, Log $log = null)
+    {
+        $this->produtos[] = $produto;
+        if(!is_null($log))
+            $log->log('Adicionando produto no carrinho');
+    }
+    public function getProdutos()
+    {
+        return $this->produtos;
+    }
+
+    public function getTotalProdutos()
+    {
+        return sizeof($this->produtos);
+    }
+    public function getTotalCompra()
+    {
+        $totalCompra = 0;
+        foreach ($this->produtos as $p) {
+            $totalCompra += $p->getPrice();
+        }
+        return $totalCompra;
+    }
+
+
+}
